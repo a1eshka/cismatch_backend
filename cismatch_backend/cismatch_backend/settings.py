fe84828144792c093e71f6cda24c160cb9eca078
Алексей <zexdesgin@gmail.com>
2025-04-14 21:43:11 +0300

    media files

--- cismatch_backend/cismatch_backend/settings.py
+++ cismatch_backend/cismatch_backend/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-print('SECRET_KEY',SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
