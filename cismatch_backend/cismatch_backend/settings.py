fcaddbd9dac91077aa281dcff00a8b3707abb8b6
Алексей <zexdesgin@gmail.com>
2025-04-15 15:46:24 +0300

    allowed_trusted_csrf

--- cismatch_backend/cismatch_backend/settings.py
+++ cismatch_backend/cismatch_backend/settings.py
@@ -85,7 +85,10 @@ AUTHENTICATION_BACKENDS = (
     # Включите allauth бэкенд
     'social_core.backends.steam.SteamOpenId',
 )
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://cismatch.ru",
+    "https://api.cismatch.ru",
+]
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
