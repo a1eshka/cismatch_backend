2fa108de60bc0d81db24a70873e48bbe228fccb8
Алексей <zexdesgin@gmail.com>
2025-04-16 15:28:57 +0300

    ad

--- cismatch_backend/cismatch_backend/urls.py
+++ cismatch_backend/cismatch_backend/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('control-panel-x98/', admin.site.urls),
     path('api/', include('post.urls')),
     path('api/teams/', include('team.urls')),
     path('api/servers/', include('server.urls')),
